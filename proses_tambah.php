056bf2fa03fa6a829917b478bda965dba6cc143f
A_wy <nursadrinasakina10@gmail.com>
2026-05-21 11:21:27 +0800

    Update proses_tambah.php

--- proses_tambah.php
+++ proses_tambah.php
@@ -8,20 +8,44 @@ $satuan    = $_POST['satuan'];
 $tgl_exp   = $_POST['tanggal_kadaluarsa'];
 $id_user   = $_SESSION['id_pengguna'];
 
-// 1. Simpan ke tabel master obat
-$query_obat = "INSERT INTO obat (nama_obat, stok, satuan, tanggal_kadaluarsa) 
-               VALUES ('$nama_obat', '$jumlah', '$satuan', '$tgl_exp')";
+// Cek apakah obat sudah ada
+$cek = mysqli_query($koneksi,
+    "SELECT * FROM obat WHERE nama_obat='$nama_obat'"
+);
 
-if(mysqli_query($koneksi, $query_obat)){
-    $id_obat_baru = mysqli_insert_id($koneksi);
-    
-    // 2. Catat riwayat ke tabel pemasukan_obat
-    $query_masuk = "INSERT INTO pemasukan_obat (id_obat, jumlah_masuk, tanggal_masuk, id_pengguna) 
-                    VALUES ('$id_obat_baru', '$jumlah', CURDATE(), '$id_user')";
-    mysqli_query($koneksi, $query_masuk);
+if(mysqli_num_rows($cek) > 0){
 
-    header("location:index.php");
-} else {
-    echo "Gagal menyimpan: " . mysqli_error($koneksi);
+    // Jika sudah ada → update stok
+    $data = mysqli_fetch_assoc($cek);
+    $id_obat = $data['id_obat'];
+
+    mysqli_query($koneksi,
+        "UPDATE obat
+         SET stok = stok + '$jumlah',
+             tanggal_kadaluarsa='$tgl_exp'
+         WHERE id_obat='$id_obat'"
+    );
+
+}else{
+
+    // Jika belum ada → insert baru
+    mysqli_query($koneksi,
+        "INSERT INTO obat
+        (nama_obat, stok, satuan, tanggal_kadaluarsa)
+        VALUES
+        ('$nama_obat', '$jumlah', '$satuan', '$tgl_exp')"
+    );
+
+    $id_obat = mysqli_insert_id($koneksi);
 }
+
+// Simpan riwayat pemasukan
+mysqli_query($koneksi,
+    "INSERT INTO pemasukan_obat
+    (id_obat, jumlah_masuk, tanggal_masuk, id_pengguna)
+    VALUES
+    ('$id_obat', '$jumlah', CURDATE(), '$id_user')"
+);
+
+header("location:index.php");
 ?>
